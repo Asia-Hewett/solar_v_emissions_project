d749d030c8745c8642fbb84af3eb8a6364733ba8
Asia Hewett <66974155+Asia-Hewett@users.noreply.github.com>
2020-08-20 22:10:17 -0500

    Revert "pull master"

--- State Solar Production Data by Year/config.py
+++
@@ -1,5 +0,0 @@
-# Enter your API key
-
-##This is Tammi's eia key
-eia_key = "89b37550d0c5ab1226f65143d9f095e7"
-##Test if it loads to github now that I fixed gitignore
---
+++ config.py
@@ -0,0 +1,3 @@
+# Enter your API key
+gkey = "TEfmHfQ1iyJLs6MVbn5a6h2atsVT5eTnuqpx2D4S"
+##This is Tammi's data.gov key
